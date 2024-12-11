6b6a21d4ff9415c42379333a9c9b035bdaafb692
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-12-11 22:59:26 +0700

    change some character class and create play class

--- Assets/Script/Character/Player.cs
+++ Assets/Script/Character/Player.cs
@@ -1,16 +1,61 @@
+using Unity.Collections;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Character
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Singleton")]
+    public static Player Instance{get; private set;}
+    
+    [Header("Player Stats")]
+    public float dodgeRate{get; private set;}
+    public float criticalRate{get; private set;}
+    public float exp;
+    public float coin;
+
+    [Header("Collectable")]
+    private float collectItemRange;
+
+    // [Header("Skill")]
+    // public Skill activeSkill1;
+    // public Skill activeSkill2;
+    // public Skill mobilitySkill;
+
+    // [Header("Inventory")]
+    // public Inventory inventory;
+    protected override void Awake()
+    {
+        if(Instance != null && Instance != this){
+            Destroy(this);
+        }else{
+            Instance = this;
+        }
+        DontDestroyOnLoad(this);
+        base.Awake();
+    }
+    void FixedUpdate()
+    {
+        PlayerInput();
+    }
+
+    protected override void Attack()
     {
         
     }
+    private void PlayerInput(){
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(x,y) * speed;
+        Move(movement);
+    }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Move(Vector2 velocity)
     {
-        
+        rb2D.linearVelocity = velocity;
+    }
+
+    protected override void Die()
+    {
+        // Do something with die mechanics
+        base.Die();
     }
 }
