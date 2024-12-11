89d8a3e61a7fd4795a034ca88579414e5e41da9b
MAXezD <gunnaput999@gmail.com>
2024-12-11 22:19:48 +0700

    QuestObjective

--- Assets/Script/Game Manager/GameManager.cs
+++ Assets/Script/Game Manager/GameManager.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+    public static event OnEnemyDeath BattleReport;
+
+    public delegate void OnEnemyDeath(Enemy enemy);
+
+    public static void onEnemyKilled(Enemy enemy)
+    {
+        GameManager.BattleReport?.Invoke(enemy);
+    }
 }
