fc6b8dd8c128c4df685ee1dfc10950b3feae585d
MAXezD <gunnaput999@gmail.com>
2024-12-12 21:05:50 +0700

    Item and Inventory

---
+++ Assets/Script/Inventory System/Inventory.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Item[] itemsArray;
+
+    public void SortItems()
+    {
+
+    }
+    public void GetItem()
+    {
+
+    }
+    public void AddItem(Item item,int count)
+    {
+
+    }
+    public void RemoveItem(Item item, int count)
+    {
+
+    }
+}
---
+++ Assets/Script/Inventory System/Item/UsableItem.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class UsableItem : Item
+{
+    public enum itemType
+    {
+
+    }
+    private int level;
+    private float cooldown;
+    
+    public void Used()
+    {
+
+    }
+}
