9ce2a80d0b66166e1da0d477ec9b9b62f33dc3ce
PEPSEALSEA <iampep2009@gmail.com>
2024-12-14 22:21:22 +0700

    Update QuestManager.cs

--- Assets/Script/Quest System/QuestManager.cs
+++ Assets/Script/Quest System/QuestManager.cs
@@ -141,4 +141,35 @@ public class QuestManager : Singleton<QuestManager>
         return null;
     }
 
+    public class sQuest
+    {
+        public string _id;
+        public string name;
+        public string description;
+        public sReward reward;
+        public List<sObjective> Objective;
+    }
+
+    public class sObjective
+    {
+        public string name;
+        public string enemyID;
+        public int requiredAmount;
+        public int currentAmount;
+    }
+
+    public class sReward
+    {
+        public string _id;
+        public string name;
+        public int coin;
+        public int xp;
+        public List<sItem> item;
+    }
+
+    public class sItem
+    {
+        public string name;
+        public int amount;
+    }
 }
