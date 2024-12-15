f9ab7c8124ec2ff1f92b9a89bd89c319b7eebec6
MAXezD <gunnaput999@gmail.com>
2024-12-15 17:54:00 +0700

    Event Manager

--- Assets/Script/Events/Event.cs
+++ Assets/Script/Events/Event.cs
@@ -1,12 +1,12 @@
+using System;
 using System.Collections;
 using UnityEngine;
 [System.Serializable]
 public abstract class Event : MonoBehaviour
 {
     public string Name;
-    public string activatedDay;
+    public EventManager.DayInWeek activatedDay;
     public int activatedHour;
-    public int activatedMinute;
     public float duration;
 
     public abstract IEnumerator StartEvent();
