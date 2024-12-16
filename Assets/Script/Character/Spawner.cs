1996c940607b6382f28f0f3ee6f935f38263221f
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-12-16 21:53:31 +0700

    aaaaaa

--- Assets/Script/Character/Spawner.cs
+++ Assets/Script/Character/Spawner.cs
@@ -9,13 +9,15 @@ public class Spawner : MonoBehaviour
     public List<string> enemyIDs;
     public int maxNearbyEnemy;
     public float requiredPlayerRange;
-    private List<Enemy> storedEnemy;
+    public List<Enemy> storedEnemy = new List<Enemy>();
 
 
     public IEnumerator SpawnEnemy()
     {
+        //Debug.Log("Start spawn function");
         while (storedEnemy.Count < maxNearbyEnemy && isPlayerInRange())
         {
+            //Debug.Log("Start spawning enemy in");
             Vector2 randomSpawnPosition = CalculateSpawnPosition();
             int randomenemy = Random.Range(0, enemyIDs.Count);
 
@@ -25,8 +27,10 @@ public class Spawner : MonoBehaviour
                this.transform)
                .GetComponent<Enemy>();
             storedEnemy.Add(enemyspawning);
+            //Debug.Log("spawning should be right now");
             yield return new WaitForSeconds(spawnDelay);
         }
+        yield return null;
     }
 
     private Vector2 CalculateSpawnPosition()
@@ -37,15 +41,47 @@ public class Spawner : MonoBehaviour
         return spawnPosition;
     }
 
-    private bool isPlayerInRange(){
-        Collider2D collider2D = Physics2D.OverlapCircle(this.transform.position , requiredPlayerRange);
-        return collider2D != null && collider2D.CompareTag("Player");
+    private bool isPlayerInRange()
+    {
+        Collider2D collider2D = Physics2D.OverlapCircle(this.transform.position, requiredPlayerRange);
+        bool isInRange = collider2D != null && collider2D.CompareTag("Player");
+        Debug.Log($"Is player in range: {isInRange}");
+        return isInRange;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player")){
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player is in Spawner Trigger");
             StartCoroutine(SpawnEnemy());
+            //Debug.Log($"Stored enemy count ={storedEnemy.Count}");
+        }
+    }
+
+    public IEnumerator ForceSpawn(){
+        while (storedEnemy.Count < maxNearbyEnemy)
+        {
+            //Debug.Log("Start spawning enemy in");
+            Vector2 randomSpawnPosition = CalculateSpawnPosition();
+            int randomenemy = Random.Range(0, enemyIDs.Count);
+
+            Enemy enemyspawning = Instantiate(EnemyDataManager.Instance.GetEnemy(enemyIDs[randomenemy]),
+             randomSpawnPosition,
+              Quaternion.identity,
+               this.transform)
+               .GetComponent<Enemy>();
+            storedEnemy.Add(enemyspawning);
+            //Debug.Log("spawning should be right now");
+            yield return new WaitForSeconds(spawnDelay);
+        }
+        yield return null;
+    }
+
+    public void ForceDie(){
+        foreach(Enemy enemy in storedEnemy){
+            storedEnemy.Remove(enemy);
+            Destroy(enemy);
         }
     }
 }
---
+++ Assets/Script/Game Manager/DatabaseManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+    #region Get
+
+    #endregion
+
+    #region Post
+
+    #endregion
+
+    #region Update
+
+    #endregion
+}
