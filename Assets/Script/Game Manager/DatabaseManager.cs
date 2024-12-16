f7b5ac6e72c8038a7f0517a45a55a00466e0f065
Patriciadesu <118905032+Patriciadesu@users.noreply.github.com>
2024-12-17 04:06:16 +0700

    God father of Get Data ***READ MY SUMMARY PLS I BEG U

--- Assets/Script/Game Manager/DatabaseManager.cs
+++ Assets/Script/Game Manager/DatabaseManager.cs
@@ -1,8 +1,74 @@
 using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
 
 public class DatabaseManager : MonoBehaviour
 {
     #region Get
+    /// <summary>
+    ///
+    /// PLS READ NA EVERYONE
+    /// 
+    /// To use you have to make OnGetData method to recieve data and assign to you variable
+    ///
+    /// OnGetData method will HAVE TO BE "VOID"
+    ///
+    /// OnGetData method NEED "ONLY 1 PARAM" AND IT HAVE TO BE YOUR DATAOBJECT CLASS
+    ///
+    /// Ex.
+    ///
+    /// public void Awake()
+    /// {
+    ///     GetDataObject<sQuest[]>(myApi,OnGetQuestData);
+    /// }
+    ///
+    /// public void OnGetQuestData(sQuest[] questDatas)
+    /// {
+    ///     if( questDatas == null) Debug.Log("ERROR WA"); return;
+    ///     myVariable = questDatas
+    ///     CreateQuest();
+    /// }
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="Api"></param>
+    /// <param name="callback"></param>
+
+    public void GetDataObejct<T>(string Api ,Action<T> callback) where T : class
+    {
+        StartCoroutine(GetData<T>(Api, callback));
+    }
+    private IEnumerator GetData<T>(string Api, Action<T> callback) where T : class
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(Api))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError($"Error while fetching data from API: {Api}, Error: {request.error}");
+                callback(null); 
+            }
+            else
+            {
+                try
+                {
+                    string jsonResponse = request.downloadHandler.text;
+                    T data = JsonConvert.DeserializeObject<T>(jsonResponse);
+
+                    callback(data);
+                }
+                catch (Exception ex) 
+                {
+                    Debug.LogError($"Make Sure your DataObject variable name match with backend na : {ex.Message}");
+                    callback(null);
+                }
+            }
+        }
+    }
 
     #endregion
 
@@ -10,7 +76,4 @@ public class DatabaseManager : MonoBehaviour
 
     #endregion
 
-    #region Update
-
-    #endregion
 }
