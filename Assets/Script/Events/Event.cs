bbd29291830c40f6bd15663195897576f4d91802
MAXezD <gunnaput999@gmail.com>
2025-01-02 21:07:56 +0700

    Clean event

--- Assets/Script/Events/Event.cs
+++ Assets/Script/Events/Event.cs
@@ -7,7 +7,7 @@ public abstract class Event : ScriptableObject
 {
     public string eventID;
     public string Name;
-    public List<EventManager.DayInWeek> activatedDay = new List<EventManager.DayInWeek>();
+    public List<EventManager.DAYINWEEK> activatedDay = new List<EventManager.DAYINWEEK>();
     [Tooltip("Time that the event start.(24H)")]public int activatedHour;
     [Tooltip("How many hours the event will last.")]public float duration;
     public abstract IEnumerator StartEvent();
