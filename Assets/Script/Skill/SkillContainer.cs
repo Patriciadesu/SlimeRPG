10b87da72facf6f8ea50b4c7521239e801f11e29
PeakyBoi <peak0041@gmail.com>
2025-01-05 17:39:26 +0700

    shopcodeidknotdone

--- Assets/Script/Skill/SkillContainer.cs
+++ Assets/Script/Skill/SkillContainer.cs
@@ -2,18 +2,38 @@ using UnityEngine;
 using TMPro;
 
 [RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+
 public class SkillContainer : MonoBehaviour , ICollectables
 {
     public Skill skill;
     public TMP_Text PriceText;
     public void Collect()
+    { GameObject player = GameObject.FindWithTag("Player");
+        Player p1 = player.GetComponent<Player>();
+        if ( p1.coin >= skill.Price)  //doesn't work
+        {
+            p1.coin -= skill.Price;
+            skill.Have = true;
+            Destroy(this.gameObject); 
+        }
+         //Player.Instance.AddSkill(skill);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        // Player.Instance.AddSkill(skill);
+        if(other.CompareTag("Player"))
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Collect();
+            }
+        }
     }
 
     void Awake()
     {
-        
+        BoxCollider2D box = GetComponent<BoxCollider2D>();
+        box.isTrigger = true;
     }
     public void GetSprite()
     {
