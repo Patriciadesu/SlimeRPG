7e23296ffe5a5250f664397637c0e0bb845989bb
PEPSEALSEA <iampep2009@gmail.com>
2025-01-08 19:05:51 +0700

    Quest

--- Assets/Script/InteractTester.cs
+++ Assets/Script/InteractTester.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class InteractTester : MonoBehaviour
 {
-    public NPC currentNPC;
+    public QuestNPC currentNPC;
     public Rigidbody2D rb2D;
     void Awake()
     {
@@ -11,7 +11,7 @@ public class InteractTester : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.TryGetComponent<NPC>(out NPC npc))
+        if (other.TryGetComponent<QuestNPC>(out QuestNPC npc))
         {
             currentNPC = npc;
         }
@@ -34,16 +34,21 @@ public class InteractTester : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    //void FixedUpdate()
+    //{
+    //    Move();
+    //}
+
+    //public void Move(){
+    //    float x = Input.GetAxisRaw("Horizontal");
+    //    float y = Input.GetAxisRaw("Vertical");
+
+    //    Vector2 movement = new Vector2(x , y);
+    //    rb2D.linearVelocity = movement * 5;
+    //}
+
+    public void GiveQuest(int index)
     {
-        Move();
-    }
-
-    public void Move(){
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        Vector2 movement = new Vector2(x , y);
-        rb2D.linearVelocity = movement * 5;
+        currentNPC.GiveQuest(index);
     }
 }
